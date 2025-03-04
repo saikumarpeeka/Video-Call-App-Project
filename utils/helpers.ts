2d495f93c57a7086d48bc40f3ba3e2f2d8d8b324
SAIKUMAR PEEKA <saikumarr.peeka@gmail.com>
2025-03-04 21:51:55 +0530

    helpers.ts

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,12 +1,11 @@
 export function randomID(len: number) {
-    let result = '';
-    if (result) return result;
-    var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
-      maxPos = chars.length,
-      i;
-    len = len || 5;
-    for (i = 0; i < len; i++) {
-      result += chars.charAt(Math.floor(Math.random() * maxPos));
+    let result = ''; 
+    var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP';
+    var maxPos = chars.length;
+    
+    for (let i = 0; i < len; i++) {
+        result += chars.charAt(Math.floor(Math.random() * maxPos));
     }
+
     return result;
-  }
+}
